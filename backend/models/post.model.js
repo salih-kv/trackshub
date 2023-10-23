8ad7be88052c2689eab09086bc4a6a93eefd2764
salih-kv <profileofsalih@gmail.com>
2023-10-23 19:03:36 +0530

    create: post model

---
+++ backend/models/post.model.js
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+import { v4 as uuidv4 } from "uuid";
+
+const postSchema = Schema(
+  {
+    _id: {
+      type: String,
+      default: () => uuidv4().slice(0, 6),
+    },
+    userId: { type: String, required: true, ref: "Users" },
+    content: {
+      type: String,
+      required: [true, "Content is required"],
+    },
+    likes: [
+      {
+        type: String,
+        ref: "Users",
+      },
+    ],
+    comments: [
+      {
+        type: String,
+        ref: "Users",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Posts = model("Posts", postSchema);
+export default Posts;
