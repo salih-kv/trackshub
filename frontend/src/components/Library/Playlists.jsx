4e590faf483ab30f9ef9714c393ffe80392c3788
salih-kv <profileofsalih@gmail.com>
2023-12-03 20:21:11 +0530

    nested routes for library

---
+++ frontend/src/components/Library/Playlists.jsx
@@ -0,0 +1,34 @@
+import { FaHeart } from "react-icons/fa6";
+import { Link, Outlet } from "react-router-dom";
+import ProfileImg from "../ProfileImg";
+
+const NavItems = [
+  { to: "", text: "My Playlists", icon: <ProfileImg w={4} /> },
+  {
+    to: "",
+    text: "Liked Playlists",
+    icon: <FaHeart className="text-red-500" />,
+  },
+];
+
+const Playlists = () => {
+  return (
+    <section className="flex justify-between w-full gap-8 pt-4">
+      <div className="flex flex-col min-w-[280px] w-1/4">
+        {NavItems?.map(({ to, text, icon }) => (
+          <Link
+            to={to}
+            key={text}
+            className="p-4 flex items-center gap-4 hover:bg-s-light dark:hover:bg-s-dark rounded-lg"
+          >
+            <span className={`p-2 rounded-lg bg-[#ffdbe5]`}>{icon}</span>
+            <span className="text-sm">{text}</span>
+          </Link>
+        ))}
+      </div>
+      <Outlet />
+    </section>
+  );
+};
+
+export default Playlists;
--- frontend/src/pages/Library.jsx
+++ frontend/src/pages/Library.jsx
@@ -1,10 +1,46 @@
+import { useEffect } from "react";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
+
+const NavLinks = [
+  {
+    to: "albums",
+    label: "Albums",
+  },
+  {
+    to: "playlists",
+    label: "Playlists",
+  },
+];
+
 const Library = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const path = window.location.pathname;
+    if (path === "/library" || path === "/library/") {
+      navigate("/library/albums");
+    }
+  }, [navigate]);
   return (
-    <div>
-      <h1>Library</h1>
-      <p>Liked Music</p>
-      <p>Liked Playlists</p>
-      <p>Communities</p>
+    <div className="flex flex-col w-full py-0 sm:py-8 lg:py-4 mx-4 lg:mx-0">
+      <div className="flex w-full gap-8">
+        <header className="w-full flex justify-center lg:justify-start gap-3 lg:gap-8 pb-2 lg:pb-4">
+          {NavLinks?.map(({ to, label }) => (
+            <div key={to} className="group/link hover:bg-slate-100">
+              <NavLink
+                to={to}
+                className="text-gray-500 text-xs lg:text-sm font-medium"
+              >
+                {label}
+              </NavLink>
+              <div className="w-8 h-[2px] bg-black dark:bg-white rounded-full invisible group-hover/link:visible"></div>
+            </div>
+          ))}
+        </header>
+      </div>
+      <div className=" w-full">
+        <Outlet />
+      </div>
     </div>
   );
 };
